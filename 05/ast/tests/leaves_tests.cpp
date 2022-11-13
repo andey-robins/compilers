ec981c7136628869327bac22390b9df1afc3d006
Andey Robins <andeyrobins@gmail.com>
2022-11-13 15:29:05 -0700

    Finish unit testing leaf nodes

--- 05/ast/tests/leaves_tests.cpp
+++ 05/ast/tests/leaves_tests.cpp
@@ -26,9 +26,11 @@ BOOST_AUTO_TEST_CASE(leafOutput)
 
     numNode->print(&s);
     BOOST_CHECK(s.str() == "NUMBER --> 12\n");
+    BOOST_CHECK(numNode->getVal() == 12);
     s.clear();
     s.str("");
 
     opNode->print(&s);
     BOOST_CHECK(s.str() == "+");
+    BOOST_CHECK(opNode->getOp() == "+");
 }
